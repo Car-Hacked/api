eb609527e187dfe37d6b8be136c53c35dc3ad761
timgass <timothyagass@gmail.com>
2020-01-25 06:07:58 -0600

    fully integrated mongodb support

--- server/api/services/garage/garages.service.js
+++ server/api/services/garage/garages.service.js
@@ -1,15 +1,24 @@
 import l from '../../../common/logger';
-import Garage from '.../.../../common/models/Garage';
+import { Garage } from '../../../common/models/Garage';
 
 class GaragesService {
-    all() {
+    async all() {
         l.info(`${this.constructor.name}.all()`);
-        return Garage.find().catch(error => error);
+        return Garage.find({}, (e, r) => r).catch(error => error);
     }
 
     byId(id) {
         l.info(`${this.constructor.name}.byId(${id})`);
         return Garage.findOne({ _id: id }).catch(error => error);
+    }
+
+    async create(body) {
+        const garageConfig = {
+            carsInLot: body.carsInLot || 0,
+            capacity: body.capacity
+        };
+        const garage = await Garage.create(garageConfig).catch(error => error);
+        return garage;
     }
 }
 
