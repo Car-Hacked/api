97ae85f12093b57f8c2b33888444adb63ba21bcb
timgass <timothyagass@gmail.com>
2020-01-25 10:08:21 -0600

    added socket.io

--- server/common/server.js
+++ server/common/server.js
@@ -44,7 +44,15 @@ export default class ExpressServer {
 
     oas(app, this.routes)
       .then(() => {
-        http.createServer(app).listen(port, welcome(port));
+        const server = http.createServer(app).listen(port, welcome(port));
+        const io = require('socket.io')(server);
+        io.on('connection', (socket) => {
+          let time = setInterval(() => {
+            let current = new Date().toTimeString();
+            socket.emit("time", { time: current });
+            console.log(`Emmited event time at ${current}.`);
+          }, 1000);
+        });
       })
       .catch(e => {
         l.error(e);
