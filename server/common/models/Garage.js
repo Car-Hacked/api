801db7994f2720b76ed74a5a305297944dc57018
Chris Rodriguez <chrisleerod@gmail.com>
2020-01-25 19:25:41 -0600

    Added address to database

--- server/common/models/Garage.js
+++ server/common/models/Garage.js
@@ -5,6 +5,10 @@ const GarageSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    address: {
+        type: String,
+        required: true
+    },
     garageNumber: {
         type: Number,
         default: 0,
